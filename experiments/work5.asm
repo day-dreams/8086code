10ad7e2f4a817ae73e637bc5f9c90db19cf78633
daydream <zn.moon2016@gmail.com>
2017-10-16 20:29:14 +0800

    添加注释

--- experiments/work5.asm
+++ experiments/work5.asm
@@ -39,36 +39,36 @@ start:;声明整个程序的开始
 
 
 
-    mov cx,array_len-1  ;i(array_len)次外层循环
-    outter_loop_begin:      ;cx=array_len-1,array_len-2,..,0
+    mov cx,array_len-1          ;i(array_len)次外层循环
+    outter_loop_begin:          ;cx=array_len-1,array_len-2,..,0
         cmp cx,0
-        jbe outter_loop_exit  ;cx<=0则结束外层循环
-                             ;否则执行内曾循环
+        jbe outter_loop_exit    ;cx<=0则结束外层循环
+                                ;否则执行内曾循环
 
-        mov dx,0      ;j(dx)次内层循环
-        inner_loop_begin:   ;dx=0,1,2,..,cx-1
+        mov dx,0                            ;j(dx)次内层循环
+        inner_loop_begin:                   ;dx=0,1,2,..,cx-1
             cmp dx,cx
-            jae inner_loop_exit ;dx>=cx时结束内曾循环
-                                ;否则进入内曾循环，进行比较交换
+            jae inner_loop_exit             ;dx>=cx时结束内曾循环
+                                            ;否则进入内曾循环，进行比较交换
             mov si,dx
             mov di,dx
             add di,1
             mov al,ds:(array+[si])
             mov bl,ds:(array+[di])
             cmp al,bl
-            ja swap    ;array[di]>=array[di+1],不需要改变位置
+            ja swap                             ;array[di]>=array[di+1],不需要改变位置
             jmp inner_loop_end
             swap:
                 mov ds:(array+[si]),bl
                 mov ds:(array+[di]),al
-        inner_loop_end:
+        inner_loop_end:                     ;dx++,进行下一次内曾循环
             add dx,1
             jmp inner_loop_begin
         inner_loop_exit:
 
-    outter_loop_end:
+    outter_loop_end:            ;cx--
         sub cx,1
-        jmp outter_loop_begin
+        jmp outter_loop_begin   ;进行下一次外层循环
     outter_loop_exit:
  
     
